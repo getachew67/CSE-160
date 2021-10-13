65c1f8329ba011161037d07e2d27cff4688c2404
Agnes Li <agnesli@uw.edu>
2021-10-13 11:59:00 -0700

    update file

--- Coding Bat/Coding-Bat-Check-In-1.py
+++ Coding Bat/Coding-Bat-Check-In-1.py
@@ -5,8 +5,22 @@
 # sum_odds([3, 6, 11, 2, 5]) → 19
 # sum_odds([]) → 0
 
-x = [5, 2, 6, 3, 4]
 
-def sum_odds(x):
-    print(sum(num for num in x if num % 2 == 1))
+def sum_odds(nums):
+    return sum(num for num in nums if num % 2 == 1)
 
+
+# Returns True if int hidden_num is found in the given array.
+# contains_hidden([5, 2, 6, 3, 4], 3) → True
+# contains_hidden([3, 6, 11, 2, 5], 19) → False
+# contains_hidden([3, 6, 11, 2, 5], 5) → True
+
+
+def contains_hidden(nums, hidden_num):
+    for num in nums:
+        if num == hidden_num:
+            return True
+    return False
+
+
+# could also just do "return hidden_num in nums"
